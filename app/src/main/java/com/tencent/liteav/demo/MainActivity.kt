57112256c2ff47da600ff28ce2b5302e11c34253
xiaoxiong1006 <758063761@qq.com>
2020-08-14 10:05:49 +0800

    Fix:change androidX component to app compat

--- app/src/main/java/com/tencent/liteav/demo/MainActivity.kt
+++ app/src/main/java/com/tencent/liteav/demo/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.tencent.liteav.demo
 
-import androidx.appcompat.app.AppCompatActivity
+//import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
