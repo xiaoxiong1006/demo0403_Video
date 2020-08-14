249e46eab2375f56b7518a0f33779f607689fc22
xiaoxiong1006 <758063761@qq.com>
2020-08-14 11:45:25 +0800

    Feature:Record

--- app/src/main/java/com/tencent/liteav/demo/MainActivity.kt
+++ app/src/main/java/com/tencent/liteav/demo/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.tencent.liteav.demo
 
 //import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tencent.liteav.demo.videorecord.TCVideoSettingActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_record.setOnClickListener{
+            var intent = Intent()
+            intent.setClass(this, TCVideoSettingActivity().javaClass)
+            startActivity(intent)
+        }
+
     }
 }
